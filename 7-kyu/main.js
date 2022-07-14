a4791b9775484b28fe7be31782183bed22e19794
htbhub <42898475+htbhub@users.noreply.github.com>
2022-07-14 14:41:49 -0700

    7.14.22

--- 7-kyu/main.js
+++ 7-kyu/main.js
@@ -1,3 +1,5 @@
+// 7 kyu problems 
+
 // Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
 
 // For example:
